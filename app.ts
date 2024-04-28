19a10ea28f339d354abd1657c58ab40c516057b7
shajjadhossanWD <shajjadhossan111@gmail.com>
2024-04-28 20:57:03 +0600

    project setup with nodejs, typescript, expressjs and mongodb

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -10,12 +11,9 @@ app.use(express.json({ limit: "50mb" }));
 // cookie parser
 app.use(cookieParser());
 
-// cors
-app.use(
-  cors({
-    origin: process.env.ORIGIN,
-  })
-);
+app.use(cors());
+
+// routes
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
@@ -31,3 +29,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+app.use(ErrorMiddleware);
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
+require("dotenv").config();
 import { app } from "./app";
 import connectDB from "./utils/db";
-require("dotenv").config();
 
 // create server
 app.listen(process.env.PORT, () => {
